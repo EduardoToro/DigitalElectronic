345a78968c68f0f8c2419818b6a622ec675157e8
EduardoToro <etorob@unal.edu.co>
2023-06-16 09:50:39 -0500

    Actualización de versiones de los códigos UART

--- Zephyr/ESP32/PZEM/src/main.c
+++ Zephyr/ESP32/PZEM/src/main.c
@@ -14,8 +14,7 @@
 
 //Variable Declaration
 uint8_t cmd_read_voltage[] = {0xF8, 0x04, 0x00, 0x00, 0x00, 0x0A, 0x64, 0x64};
-uint8_t cmd_read_current[] = {0xB1, 0xC0, 0xA8, 0x01, 0x01, 0x00, 0x1B};
-uint8_t rx_buf[8];
+uint8_t rx_buf[25];
 float voltage, current;
 
 /* change this to any other UART peripheral if desired */
@@ -32,25 +31,26 @@ void main(void)
 
 	while (1)
 	{
-		int ret_tx = uart_tx(uart_dev, cmd_read_voltage, sizeof(cmd_read_voltage), SYS_FOREVER_US); 
+		int ret_tx = uart_tx(uart_dev, cmd_read_voltage, sizeof(cmd_read_voltage), SYS_FOREVER_MS); 
 		if (ret_tx != 0)
 		{
 			printk("Fallo el envio de datos!\n"); 
 		}
-
-		printf("TX1: %d", cmd_read_voltage[1]); 
+		printf("TX0: %d", cmd_read_voltage[0]);
+		printf(" = TX1: %d", cmd_read_voltage[1]); 
 		printf(" = TX2: %d", cmd_read_voltage[2]); 
 		printf(" = TX3: %d", cmd_read_voltage[3]); 
 		printf(" = TX4: %d\n", cmd_read_voltage[4]); 
 		
 
-		int ret_rx = uart_rx_enable(uart_dev, rx_buf, sizeof(rx_buf), 1000);
+		int ret_rx = uart_rx_enable(uart_dev, rx_buf, sizeof(rx_buf), SYS_FOREVER_MS);
 		if (ret_rx != 0)
 		{
 			printk("Fallo la recepción de datos!\n"); 
 		}
 
-		printf("RX1: %d", rx_buf[1]); 
+		printf("RX0: %d", rx_buf[0]); 
+		printf(" = RX1: %d", rx_buf[1]); 
 		printf(" = RX2: %d", rx_buf[2]); 
 		printf(" = RX3: %d", rx_buf[3]); 
 		printf(" = RX4: %d\n", rx_buf[4]); 
